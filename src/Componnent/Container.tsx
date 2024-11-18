88b9502eb86c7600b148daf8cb91be6a4f427e12
Ehsanmadadi <ehsanmadadialengareqhi@gmail.com>
2024-11-18 11:07:04 +0330

    add scroldown

---
+++ src/Componnent/Container.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Container() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>Container</div>
+  )
+}
--- src/Page/FerstPage.tsx
+++ src/Page/FerstPage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import BgEmam from '../Componnent/BgEmam'
+import React, { useRef } from 'react';
+import BgEmam from '../Componnent/BgEmam';
+import Container from '../Componnent/Container';
 
 export default function FerstPage() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleScrollDown = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <div className='w-full h-full overflow-auto'>
-        <BgEmam/>
-        <div className='bottom-0 mx-auto flex items-center'></div>
+    <div className="w-full h-full overflow-auto">
+      <BgEmam onScrollDown={handleScrollDown} />
+      <div ref={containerRef}>
+        <Container />
+      </div>
     </div>
-  )
+  );
 }

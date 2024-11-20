956f5d25bd28a28a9a81fe36c9d1808ff7a0af91
Ehsanmadadi <ehsanmadadialengareqhi@gmail.com>
2024-11-20 08:40:59 +0330

    add font and headerContainer

--- src/Componnent/Container.tsx
+++ src/Componnent/Container.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import HeaderContainer from "./HeaderContainer";
 
 export default function Container() {
   return (
-    <div className='w-full h-screen flex justify-center items-center'>Container</div>
-  )
+    <div className="w-full h-screen mt-10">
+      <HeaderContainer/>
+    </div>
+  );
 }
